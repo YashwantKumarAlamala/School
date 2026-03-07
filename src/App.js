8087f034e98a92fd13424c18ef0a3931cda4a303
Yashwant Anagha Engineers <yashwantkumar@anaghaengineers.com>
2026-03-07 14:01:46 +0530

    pictures added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import Navbar from "./essentials/Navbar";
 import HeroSection from "./essentials/HeroSection";
